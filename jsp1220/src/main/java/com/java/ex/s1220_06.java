0d6270c477811d864aca4306f70b4f8d5b739307
eddy961206 <89622709+eddy961206@users.noreply.github.com>
2022-12-20 22:44:40 +0900

    Update s1220_06.java

--- jsp1220/src/main/java/com/java/ex/s1220_06.java
+++ jsp1220/src/main/java/com/java/ex/s1220_06.java
@@ -1,6 +1,9 @@
 package com.java.ex;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +14,43 @@ import javax.servlet.http.HttpServletResponse;
 public class s1220_06 extends HttpServlet {
 	
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name="", id="", pw="", pwChk="", email="", addr1="", addr2="", gender="", grade="";
+		String[] hobbys = null;
+		
 		System.out.println("doAction");
+		request.setCharacterEncoding("utf-8");
+		
+		name = request.getParameter("name");
+		id = request.getParameter("id");
+		pw = request.getParameter("pw");
+		pwChk = request.getParameter("pwChk");
+		email = request.getParameter("email");
+		addr1 = request.getParameter("addr1");
+		addr2 = request.getParameter("addr2");
+		gender = request.getParameter("gender");
+		hobbys = request.getParameterValues("hobbys");
+		grade = request.getParameter("grade");
+		
+		System.out.println("이름 : "+name);
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html><head>");
+		writer.println("<style>table,td{border:1px solid black; border-collapse:collapse;}</style>");
+		writer.println("</head><body>");
+		writer.println("<h2>회원정보</h2>");
+		writer.println("<table><tr><td>name</td><td>"+ name +"</td></tr>");
+		writer.println("<tr><td>id</td><td>"+ id +"</td></tr>");
+		writer.println("<tr><td>pw</td><td>"+ pw +"</td></tr>");
+		writer.println("<tr><td>email</td><td>"+ email +"</td></tr>");
+		writer.println("<tr><td>gender</td><td>"+ gender +"</td></tr>");
+		writer.println("<tr><td>address1</td><td>"+ addr1 +"</td></tr>");
+		writer.println("<tr><td>address2</td><td>"+ addr2 +"</td></tr>");
+		writer.println("<tr><td>hobbys</td><td>"+ Arrays.toString(hobbys) +"</td></tr>");
+		writer.println("<tr><td>grade</td><td>"+ grade +"</td></tr>");
+		writer.println("</table></body></html>");
+		
+		writer.close();
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
